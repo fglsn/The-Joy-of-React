f94a094e8e2fe2df62c9b0b34b0cfb3d29de821a
Ilona Shakurova <leverseau19@gmail.com>
2023-03-14 14:26:15 +0200

    Add missing letter

--- wordle/src/components/Keyboard/Keyboard.js
+++ wordle/src/components/Keyboard/Keyboard.js
@@ -14,7 +14,7 @@ const getLetterStatus = (guesses) => {
 
 const Keyboard = ({ guesses }) => {
 	const rows = [
-		['Q', 'W', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+		['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
 		['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
 		['Z', 'X', 'C', 'V', 'B', 'N', 'M']
 	];
